bce5264a3e2773ef329056b1ceeb960cb834d419
Rogerio Siqueira <rogercomp@gmail.com>
2019-11-19 20:28:11 -0300

    Heroku API

--- app/src/main/java/com/ddm/iclean/services/RetrofitService.java
+++ app/src/main/java/com/ddm/iclean/services/RetrofitService.java
@@ -13,7 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitService {
 
     private static Context context;
-    private String baseUrl = "http://localhost:8080/";
+    private String baseUrl = "https://prjiclean.herokuapp.com/";
     private InterfaceDeServicos api;
     private static RetrofitService instancia;
 
