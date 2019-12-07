ce162fde6dcb735a12f61b221c719e0a53e1c1f2
Marco Antonio <marcoantonio@gmail.com>
2019-12-07 18:09:53 -0300

    tentando criar ordem de servico ~

--- app/src/main/java/com/ddm/iclean/activities/DetalheAnuncioActivity.java
+++ app/src/main/java/com/ddm/iclean/activities/DetalheAnuncioActivity.java
@@ -3,17 +3,19 @@ package com.ddm.iclean.activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.format.DateFormat;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ddm.iclean.R;
-import com.ddm.iclean.dto.DtoUser;
+import com.ddm.iclean.dto.DtoOrdemServico;
 import com.ddm.iclean.services.RetrofitService;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,5 +38,29 @@ public class DetalheAnuncioActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.descricaoAnuncio)).setText("descrição: "+descricao);
         ((TextView)findViewById(R.id.precoAnuncio)).setText("Preço: "+preco);
         ((TextView)findViewById(R.id.tituloAnuncio)).setText(titulo+"");
+    }
+
+    public void contratarOrdemServico(View view) {
+        SharedPreferences sp = getSharedPreferences("dados", 0);
+        String token = sp.getString("token", null);
+        Long id = sp.getLong("id", Long.valueOf("1"));
+
+        Date currentTime = Calendar.getInstance().getTime();
+        DateFormat df = new DateFormat();
+        
+
+        DtoOrdemServico dtoOrdemServico = new DtoOrdemServico( currentTime ,0,0,Long.valueOf("1"),Long.valueOf("1"),Long.valueOf("1"));
+
+        RetrofitService.getServico(this).criarOrdemServicos(dtoOrdemServico,token).enqueue(new Callback<DtoOrdemServico>() {
+            @Override
+            public void onResponse(Call<DtoOrdemServico> call, Response<DtoOrdemServico> response) {
+                System.out.println("deu bom");
+            }
+
+            @Override
+            public void onFailure(Call<DtoOrdemServico> call, Throwable t) {
+                System.out.println("Deu ruim");
+            }
+        });
     }
 }
